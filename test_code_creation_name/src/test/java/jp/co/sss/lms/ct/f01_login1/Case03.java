ba00d169007cf005b2bece612e2189111c253ef1
NishimuraTaiki <NishimuraTaiki@192.168.3.125>
2026-09-25 15:14:12 +0900

    add:Case03.java 西村太希 ログイン成功時の遷移テストコード追加
    タイトルの値を取得し、期待値通りかどうか確認するテストを作成。
    期待値は以下の通り
    タイトル：タイトル：コース詳細 | LMS
    
    add:Case04.java 西村太希 よくある質問画面への遷移テストコード追加
    
    メッセージの値を取得し、期待値通りかどうか確認するテストを作成。
    期待値は以下の通り
    タイトル：タイトル：よくある質問 | LMS

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -36,6 +38,15 @@ public class Case03 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		// 画面遷移
+		goTo("http://localhost:8080/lms");
+
+		// Titleの取得とアサーション
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
@@ -43,6 +54,27 @@ public class Case03 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		// ログイン画面を開く
+		goTo("http://localhost:8080/lms");
+
+		// ログインIDを入力
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+
+		// パスワードを入力
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA01");
+
+		// 「ログイン」ボタンを押下
+		webDriver.findElement(By.cssSelector("input[type='submit']")).click();
+
+		// 3秒待機
+		pageLoadTimeout(3);
+
+		// Titleの取得とアサーション
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト よくある質問機能
@@ -36,6 +43,15 @@ public class Case04 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		// 画面遷移
+		goTo("http://localhost:8080/lms");
+
+		// Titleの取得とアサーション
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
@@ -43,6 +59,27 @@ public class Case04 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		// ログイン画面を開く
+		goTo("http://localhost:8080/lms");
+
+		// ログインIDを入力
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+
+		// パスワードを入力
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA01");
+
+		// 「ログイン」ボタンを押下
+		webDriver.findElement(By.cssSelector("input[type='submit']")).click();
+
+		// 3秒待機
+		pageLoadTimeout(3);
+
+		// Titleの取得とアサーション
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
@@ -50,6 +87,53 @@ public class Case04 {
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
 		// TODO ここに追加
+		// 「機能」を取得
+		By functionMenu = By.xpath(
+				"//a[contains(@class,'dropdown-toggle') and normalize-space()='機能']");
+
+		// 3秒待機
+		visibilityTimeout(functionMenu, 3);
+
+		// 「機能」を押下
+		JavascriptExecutor js = (JavascriptExecutor) webDriver;
+		js.executeScript(
+				"arguments[0].click();",
+				webDriver.findElement(functionMenu));
+
+		// 「機能」が開くまで待機
+		new WebDriverWait(webDriver, Duration.ofSeconds(3))
+				.until(ExpectedConditions.attributeToBe(
+						functionMenu,
+						"aria-expanded",
+						"true"));
+
+		// エビデンス取得(デバッグ用)
+		// getEvidence(new Object() {
+		// });
+
+		// 「ヘルプ」を取得
+		By helpMenu = By.xpath(
+				"//ul[contains(@class,'dropdown-menu')]//a[normalize-space()='ヘルプ']");
+
+		// 3秒待機
+		visibilityTimeout(helpMenu, 3);
+
+		// 「ヘルプ」を取得
+		var helpElement = webDriver.findElement(helpMenu);
+
+		// 「ヘルプ」を押下
+		js.executeScript("arguments[0].click();", helpElement);
+
+		// ヘルプ画面への遷移を待機
+		new WebDriverWait(webDriver, Duration.ofSeconds(3))
+				.until(ExpectedConditions.titleIs("ヘルプ | LMS"));
+
+		// Titleの取得とアサーション
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
@@ -57,6 +141,34 @@ public class Case04 {
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
 		// TODO ここに追加
+		// 「よくある質問」を取得
+		By faqLink = By.xpath("//a[normalize-space()='よくある質問']");
+
+		// 3秒待機
+		visibilityTimeout(faqLink, 3);
+
+		// 「よくある質問」を押下
+		//webDriver.findElement(faqLink).click();
+		JavascriptExecutor js = (JavascriptExecutor) webDriver;
+		js.executeScript("arguments[0].click();",
+				webDriver.findElement(faqLink));
+
+		// ウィンドウ/タブの個数が2つになるまで待機
+		new WebDriverWait(webDriver, Duration.ofSeconds(3))
+				.until(ExpectedConditions.numberOfWindowsToBe(2));
+
+		// すべてのウィンドウハンドルを取得
+		Object[] newWindow = webDriver.getWindowHandles().toArray();
+
+		// 新しく開いたタブへ切り替え
+		webDriver.switchTo().window((String) newWindow[1]);
+
+		// Titleの取得とアサーション
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
